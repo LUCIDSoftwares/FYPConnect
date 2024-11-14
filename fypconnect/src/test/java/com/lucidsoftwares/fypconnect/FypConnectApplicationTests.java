7bbd1e2d81a938b80c86aacd835a567a773cbdf3
Siddique Ahmad Ryan <ryan.ahmad2923@gmail.com>
2024-11-14 19:36:22 +0500

    added springboot project

---
+++ fypconnect/src/test/java/com/lucidsoftwares/fypconnect/FypConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucidsoftwares.fypconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FypConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
