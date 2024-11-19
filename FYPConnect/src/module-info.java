b2e40a8b93dbac1eaabb2863e4b7f6c2df4b886c
blueork <114741623+blueork@users.noreply.github.com>
2024-11-20 01:14:04 +0500

    Update module-info.java
    Make application.controller accessible to javafx.

--- FYPConnect/src/module-info.java
+++ FYPConnect/src/module-info.java
@@ -1,5 +1,10 @@
 module FYPConnect {
 	requires javafx.controls;
+	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens application.controller to javafx.graphics, javafx.fxml;
 }
