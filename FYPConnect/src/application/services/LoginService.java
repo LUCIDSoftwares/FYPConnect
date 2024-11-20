d8e4a4b5b554b68453beea4a05974f8ff0ff942d
blueork <114741623+blueork@users.noreply.github.com>
2024-11-20 10:09:44 +0500

    Add functionality to LoginService
    LoginService is responsible for implementing the business logic for login

--- FYPConnect/src/application/services/LoginService.java
+++ FYPConnect/src/application/services/LoginService.java
@@ -1,5 +1,19 @@
 package application.services;
 
-public class LoginService {
+import application.datamodel.User;
+import application.persistance.DBHandler;
 
+public class LoginService {
+	DBHandler dbHandler;
+	
+	
+	public User login(String username, String password) {
+		
+		dbHandler = new DBHandler();
+		User user = this.dbHandler.retrieveUser(username, password);
+		
+		return user;
+	}
+	
+	
 }
