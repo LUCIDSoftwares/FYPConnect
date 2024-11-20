ac64eec3a6ba4f9e9f60e80c8c6f17e57119c286
blueork <114741623+blueork@users.noreply.github.com>
2024-11-20 10:08:58 +0500

    Functionality related to adminHomeScreen

--- FYPConnect/src/application/controller/LoginScreenController.java
+++ FYPConnect/src/application/controller/LoginScreenController.java
@@ -1,5 +1,45 @@
 package application.controller;
 
-public class LoginScreenController {
+import application.services.LoginService;
 
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginScreenController {
+	
+	@FXML
+	public TextField usernameTextField;
+	
+	@FXML
+	public TextField passwordTextField;
+	
+	LoginService loginService;
+	
+	public LoginScreenController() {
+		
+    }
+	
+	public void login(ActionEvent e) {
+		
+		if(usernameTextField.getText() == null || usernameTextField.getText().isEmpty()) {
+			System.out.println("User Id is empty");
+			return;
+		}
+		if(passwordTextField.getText() == null || passwordTextField.getText().isEmpty()) {
+			System.out.println("password is empty");
+			return;
+		}
+		
+		
+		
+		
+	}
+	
 }
