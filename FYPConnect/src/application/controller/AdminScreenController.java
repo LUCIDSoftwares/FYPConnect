542abebef88dc109accd1069df25a5ec3e2fd4cb
Siddique Ahmad Ryan <ryan.ahmad2923@gmail.com>
2024-11-21 15:56:24 +0500

    add logout functionality in admin dashboard

--- FYPConnect/src/application/controller/AdminScreenController.java
+++ FYPConnect/src/application/controller/AdminScreenController.java
@@ -2,14 +2,23 @@ package application.controller;
 
 import application.datamodel.*;
 import application.session.UserSession;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 public class AdminScreenController {
     private User user;
     @FXML
     public TextArea admin_username;
 
+    @FXML
+    public Button logoutButton;
     // The initialize method is automatically called after the FXML file is loaded
     @FXML
     public void initialize() {
@@ -23,4 +32,21 @@ public class AdminScreenController {
             System.out.println("No user found in session");
         }
     }
+
+    @FXML
+    public void handleLogout(ActionEvent e) {
+        try {
+            // Clear the current user session
+            UserSession.getInstance().setCurrentUser(null);
+            
+			Parent root = FXMLLoader.load(getClass().getResource("/resources/views/loginScreen.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+			stage.setScene(scene);
+			stage.show();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
