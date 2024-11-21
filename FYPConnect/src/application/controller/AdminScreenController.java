85ef6c25efb766fe9e5c298c711eb32182a9fd07
Siddique Ahmad Ryan <ryan.ahmad2923@gmail.com>
2024-11-21 16:47:53 +0500

    update admin dashboard

--- FYPConnect/src/application/controller/AdminScreenController.java
+++ FYPConnect/src/application/controller/AdminScreenController.java
@@ -10,15 +10,41 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class AdminScreenController {
     private User user;
+    
+    @FXML
+    public BorderPane bp;
+    
+    @FXML
+    public AnchorPane ap;
+    
     @FXML
     public TextArea admin_username;
 
     @FXML
     public Button logoutButton;
+    
+    @FXML
+    public Button HomeButton;
+    
+    @FXML
+    public Button CreateUsersButton;
+    
+    @FXML
+    public Button UpdateUsersButton;
+    
+    @FXML
+    public Button DeleteUsersButton;
+    
+    @FXML
+    public Button DisplayUsersButton;
+    
     // The initialize method is automatically called after the FXML file is loaded
     @FXML
     public void initialize() {
@@ -36,6 +62,7 @@ public class AdminScreenController {
     @FXML
     public void handleLogout(ActionEvent e) {
         try {
+        	System.out.println("Logout Button Clicked");
             // Clear the current user session
             UserSession.getInstance().setCurrentUser(null);
             
@@ -48,5 +75,43 @@ public class AdminScreenController {
             ex.printStackTrace();
         }
     }
+    
+    @FXML
+    public void home(MouseEvent me) {
+    	System.out.println("Home Button Clicked");
+    }
+    
+    @FXML
+	public void createUsers(MouseEvent me) {
+    	System.out.println("Create Users Button Clicked");
+	}
+    
+    @FXML
+        public void updateUsers(MouseEvent me) {
+    	System.out.println("Update Users Button Clicked");
+    }
+    
+    @FXML
+	public void deleteUsers(MouseEvent me) {
+System.out.println("Delete Users Button");
+	}
+    
+    @FXML
+        public void displayUsers(MouseEvent me) {
+    		System.out.println("Display Users Button");
+    }
+    
+	private void loadPage(String page) {
+		try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/views/" + page + ".fxml"));
+            bp.setCenter(root);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+	}
+    
+    
+    
+    
 
 }
