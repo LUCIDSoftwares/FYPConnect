68b6bbe512fda8b99096aa6062271c0ca7962fdf
blueork <114741623+blueork@users.noreply.github.com>
2024-11-22 22:07:40 +0500

    Add func to User Factory
    Allow the factory to return a list of users from the passed result set.

--- FYPConnect/src/application/services/ConcreteUserFactory.java
+++ FYPConnect/src/application/services/ConcreteUserFactory.java
@@ -2,6 +2,7 @@ package application.services;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import application.datamodel.Admin;
 import application.datamodel.Faculty;
@@ -106,6 +107,73 @@ public class ConcreteUserFactory extends UserFactory {
 			user = new Admin(name, username, password, email); 
 		}
 		return user;
+	}
+
+	
+	@Override
+	public ArrayList<User> createUserArrayList(String userType, ResultSet result) {
+		ArrayList<User> userArrayList = null;
+		
+		// used for identifying user type
+		// done so as to avoid using string comparison function repeatedly. Should in my opinion be better for performance
+		int type;
+		if(userType.equalsIgnoreCase("Student"))
+			type = 0;
+		else if(userType.equalsIgnoreCase("Faculty"))
+			type = 1;
+		else
+			type = 2;
+		
+		try {
+			// read the first row
+			if(result.next()) {
+				userArrayList = new ArrayList<>();
+				User user = null;
+	            int id = result.getInt("ID");
+	            String name = result.getString("name");
+	            String password = result.getString("password");
+	            String username = result.getString("username");
+	            String email = result.getString("email");
+	            
+	            if(type == 0) {
+		            double cgpa = result.getDouble("cgpa");
+		            user = new Student(id, name, password, username, email, cgpa);
+	            }
+	            else if(type == 1)
+		            user = new Faculty(id, name, password, username, email);
+	            else
+		            user = new Faculty(id, name, password, username, email);
+	            
+	            userArrayList.add(user);
+				
+	            // now read the remaining list of rows
+				while(result.next()) {
+					user = null;
+					id = result.getInt("ID");
+		            name = result.getString("name");
+		            password = result.getString("password");
+		            username = result.getString("username");
+		            email = result.getString("email");
+		            
+		            if(type == 0) {
+			            double cgpa = result.getDouble("cgpa");
+			            user = new Student(id, name, password, username, email, cgpa);
+		            }
+		            else if(type == 1)
+			            user = new Faculty(id, name, password, username, email);
+		            else
+			            user = new Faculty(id, name, password, username, email);
+		            
+		            userArrayList.add(user);
+				}
+				
+			}
+		} catch (SQLException e) {
+			System.out.println("Exception thrown in the createUserList(String, ResultSet) method of the ConcreteUserFactory Class");
+			e.printStackTrace();
+		}
+		
+		return userArrayList;
 	}
 	
 }
--- FYPConnect/src/application/services/UserFactory.java
+++ FYPConnect/src/application/services/UserFactory.java
@@ -1,6 +1,7 @@
 package application.services;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import application.datamodel.User;
 
@@ -8,7 +9,9 @@ import application.datamodel.User;
 public abstract class UserFactory {
 
 	public abstract User createUser(ResultSet result);
+	public abstract ArrayList<User> createUserArrayList(String userType, ResultSet result);
 	public abstract User createUser(String userType);
 	public abstract User createUser(String userType, String name, String username, String password, String email, double cgpa);
+	
 }
 
